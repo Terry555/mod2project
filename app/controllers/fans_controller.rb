b333318467ed52e5b3962e78dc7129aad941a3c0
trisjones <trisjones04@gmail.com>
2018-08-21 11:01:42 -0400

    Created show page and creation form for fans

--- app/controllers/fans_controller.rb
+++ app/controllers/fans_controller.rb
@@ -6,7 +6,21 @@ class FansController < ApplicationController
 
   def new
     @fan = Fan.new
-    render :new
+  end
+
+  def create
+    @fan = Fan.create(fan_params)
+    redirect_to @fan
+  end
+
+  def show
+    @fan = Fan.find(params[:id])
+  end
+
+  private
+
+  def fan_params
+    params.require(:fan).permit(:name, :drunk, :balance, :age, :favorite_artist)
   end
 
 end
