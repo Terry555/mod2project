f2234bb78a6d62f477620f833f6f9cd0fa004d78
trisjones <trisjones04@gmail.com>
2018-08-21 09:39:34 -0400

    The database is working properly and we're starting to build out ticket model/index

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,4 +1,10 @@
 class Ticket < ApplicationRecord
   belongs_to :fan
   belongs_to :concert
+
+  def concert_artist
+    @concert = Concert.all.find{|concert| concert.id == self.concert_id}
+    @concert.artist_name
+  end
+
 end
