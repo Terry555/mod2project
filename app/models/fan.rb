ca9f5ee4b6c9c20e9c950b056de659ea7a1f5a50
trisjones <trisjones04@gmail.com>
2018-08-24 15:00:08 -0400

    Validations

--- app/models/fan.rb
+++ app/models/fan.rb
@@ -3,4 +3,13 @@ class Fan < ApplicationRecord
   has_many :concerts, through: :tickets
   has_secure_password
 
+  validates :username, uniqueness: true
+  validates :password, length: {minimum: 3}
+  validates :password, length: {maximum: 25}
+  validates :balance, numericality: {less_than: 400}
+  validates :name, presence: true
+  validates :name, length: {maximum: 25}
+  validates :age, numericality: {maximum: 99}
+  validates :favorite_artist, length: {maximum: 35}
+
 end
